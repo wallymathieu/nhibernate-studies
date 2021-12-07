69292fde689b1e90733628fbc2c7206ba561d22c
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2021-12-07 08:14:28 +0200

    Upgrade to net6

--- web/Web/Program.cs
+++ web/Web/Program.cs
@@ -1,35 +1,35 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace SomeBasicNHApp
 {
+    ///
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
+        ///
+        public static void Main(string[] args) => BuildWebHost(args).Run();
+        ///
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .ConfigureAppConfiguration((builderContext, conf) =>
                 {
-                    //.SetBasePath(env.ContentRootPath)
-                    conf.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                        .AddJsonFile("appsettings.user.json", optional: true, reloadOnChange: true)
+                    conf.AddJsonFile("appsettings.json", reloadOnChange: true, optional: true)
+                        .AddJsonFile("appsettings.user.json", reloadOnChange: true, optional: true)
                         .AddEnvironmentVariables();
-                    if (args != null)
-                    {
-                        conf.AddCommandLine(args);
-                    }
+                    conf.AddCommandLine(args);
                 })
+                .ConfigureLogging((hostingContext, logging) => { logging.AddConsole().AddDebug(); })
                 .UseDefaultServiceProvider((context, options) =>
                 {
                     options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
                 })
+#if DEBUG
                 .CaptureStartupErrors(true)
+#endif
                 .Build();
     }
 }
