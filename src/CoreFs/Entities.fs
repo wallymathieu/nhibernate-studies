bafe90f4086c20552db7370438f398dc367e4757
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-10-04 08:26:52 +0200

    Only properties

--- src/CoreFs/Entities.fs
+++ src/CoreFs/Entities.fs
@@ -16,35 +16,25 @@ type [<AllowNullLiteral>] Order(id,orderDate,customer,version) =
 
 and [<AllowNullLiteral>] Customer(id,firstname,lastname,version)=
     new()=Customer(0,"","",0)
-    abstract Id : int with get, set
-    default val Id =id with get, set
+    member val Id =id with get, set
     
-    abstract Version : int with get, set
-    default val Version =version with get, set
+    member val Version =version with get, set
     
-    abstract Firstname : string with get, set
-    default val Firstname =firstname with get, set
+    member val Firstname =firstname with get, set
     
-    abstract Lastname : string with get, set
-    default val Lastname =lastname with get, set
+    member val Lastname =lastname with get, set
 
-    abstract Orders : IList<Order> with get, set
-    default val Orders = List<Order>() :>IList<_> with get, set
+    member val Orders = List<Order>() :>IList<_> with get, set
 and [<AllowNullLiteral>] Product(id,cost,name,version)=
     new()=Product(0,0.0,"",0)
-    abstract Id : int with get, set
-    default val Id =id with get, set
+    member val Id =id with get, set
     
-    abstract Version : int with get, set
-    default val Version =version with get, set
+    member val Version =version with get, set
 
-    abstract Cost : float with get, set
-    default val Cost =cost with get, set
+    member val Cost =cost with get, set
     
-    abstract Name : string with get, set
-    default val Name = name with get, set
+    member val Name = name with get, set
     
-    abstract Products : IList<Product> with get, set    
-    default val Products = List<Product>() :>IList<_> with get, set
+    member val Products = List<Product>() :>IList<_> with get, set
 
 
